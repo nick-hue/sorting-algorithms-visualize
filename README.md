fb5215cce079991c78eabb413345886290cef180
nick-hue <nickagelid@gmail.com>
2025-08-26 20:56:03 +0300

    moved screenshots

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 A simple interactive project that demonstrates how different sorting algorithms work by visualizing each step of the sorting process.
 
+## Screenshots
+
+### Quick Sort Final Result
+
+### Bubble Sort 50 Elements Result
+
+### Quick Sort 100 Elements Result Paused
+
--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@
 
 int main(){
 
-    int num_elements = 20;
+    int num_elements = 50;
     int max_value = 100;
     std::vector<int> numbers = getRandomNumbers(num_elements, max_value);
-    std::string algorithm = "quick"; 
+    std::string algorithm = "bubble"; 
 
     // Instead of using a function pointer, use std::function for sortFunc to allow lambdas
     using VisualizeStepCallback = std::function<void(const std::vector<int>&, bool)>;
